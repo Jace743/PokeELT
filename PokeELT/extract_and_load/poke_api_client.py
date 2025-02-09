087459a2a3839b04ed6c3b6c9cb068d13fa0bccd
Jace743 <jasreidan@JasonsPC>
2025-02-09 18:10:27 -0500

    Documentation of functions

--- PokeELT/extract_and_load/poke_api_client.py
+++ PokeELT/extract_and_load/poke_api_client.py
@@ -2,7 +2,7 @@ import duckdb
 import os
 import requests
 import yaml
-from utils import current_timestamp
+from utils import current_timestamp_utc
 
 
 class PokeApiClient:
@@ -29,6 +29,13 @@ class PokeApiClient:
         self.max_paginated_list_size: int = max_paginated_list_size
 
     def get_and_load_resource(self, resource_name: str) -> None:
+        """Executes all necessary get requests to extract all records from a resource and loads the raw json into
+        the duckdb database, with some additional metadata (request and load timestamps, record source URL).
+
+        Args:
+            resource_name (str): The name of the resource to be extracted (e.g., 'pokemon').
+
+        """
         print(f"Getting and loading resource: {resource_name}")
         resource_id_list: list[int] = self._get_resource_id_list(
             resource_name=resource_name
@@ -51,13 +58,13 @@ class PokeApiClient:
             print(f"Created raw_{resource_name} table. Retrieving and loading individual records now...")
 
             for resource_id in resource_id_list:
-                requested_at_utc = current_timestamp()
+                requested_at_utc = current_timestamp_utc()
                 request_url: str = f"{self.api_url}{resource_name}/{resource_id}/"
                 res: requests.Response = requests.get(url=request_url)
                 if res.status_code != 200:
                     res.raise_for_status()
                 else:
-                    loaded_at_utc = current_timestamp()
+                    loaded_at_utc = current_timestamp_utc()
                     conn.execute(
                         f"""
                         insert into raw_{resource_name} (id, raw_data, _requested_at_utc, _loaded_at_utc, _request_url)
@@ -75,6 +82,15 @@ class PokeApiClient:
         print(f"Successfully loaded {len(resource_id_list):,} records into raw_{resource_name}!")
 
     def get_api_spec(self, url: str, file_path: str) -> dict:
+        """Retrieve's an already-existing local API spec if it's available, and pulls it from the PokeAPI GitHub repo if it isn't.
+
+        Args:
+            url (str): URL to the Open API spec in the PokeAPI GitHub repo.
+            file_path (str): Local path to the Open API spec that may have already been downloaded.
+
+        Returns:
+            dict: Open API spec for PokeAPI.
+        """
         if not os.path.isfile(file_path):
             file_output_dir: str = os.path.dirname(file_path)
             if not os.path.exists(file_output_dir):
@@ -91,6 +107,14 @@ class PokeApiClient:
         return self._load_local_api_spec(file_path=file_path)
 
     def _load_local_api_spec(self, file_path: str) -> dict:
+        """Helper function that loads a local Open API spec if it exists.
+
+        Args:
+            file_path (str): Path to API spec.
+
+        Returns:
+            dict: The API spec.
+        """
         with open(file=file_path, mode="r") as f:
             api_spec: dict = yaml.safe_load(f)
 
@@ -99,6 +123,18 @@ class PokeApiClient:
     def _get_resource_id_list(
         self, resource_name: str, limit: int = None, offset: int = 0, announce_record_count: bool = True,
     ) -> list[int]:
+        """Retrieves a list of all resource ID values for a resource. This is necessary, because the ID's
+        don't increment in a consistent manner.
+
+        Args:
+            resource_name (str): Name of the resource for which to pull ID's.
+            limit (int, optional): Maximum number of records to contain in a list pagination, passed as a request url parameter. Defaults to None.
+            offset (int, optional): Starting point for subsequent pages, if needed; passed in a request url parameter. Defaults to 0.
+            announce_record_count (bool, optional): Whether to print additional info during debugging. Will be removed for proper logs in a future version. Defaults to True.
+
+        Returns:
+            list[int]: List of ID's for this resource.
+        """
         if not limit:
             limit = self.max_paginated_list_size
         resource_list_url: str = self.api_url + resource_name + "/"
--- PokeELT/extract_and_load/utils.py
+++ PokeELT/extract_and_load/utils.py
@@ -1,5 +1,11 @@
 import datetime as dt
 
 
-def current_timestamp():
+def current_timestamp_utc() -> str:
+    """Returns the current timestamp (in UTC).
+
+    Returns:
+        str: The current timestamp in UTC.
+    """
+
     return dt.datetime.now(dt.timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
